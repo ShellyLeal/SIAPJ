53c7bd8a90551c946bd2db4164b023283e249462
Gabriela Tourinho <gabrielantourinho@gmail.com>
2017-04-14 01:38:43 -0300

    JavaEmailApp atualizado

--- src/JavaEmailApp.java
+++ src/JavaEmailApp.java
@@ -11,6 +11,42 @@ import javax.mail.internet.MimeMessage;
 
 public class JavaEmailApp
 {
+	
+	public void enviaEmail(String address){
+		 Properties props = new Properties();
+         /** Parâmetros de conexão com servidor Gmail */
+         props.put("mail.smtp.host", "smtp.gmail.com");
+         props.put("mail.smtp.socketFactory.port", "465");
+         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+         props.put("mail.smtp.auth", "true");
+         props.put("mail.smtp.port", "465");
+
+         Session session = Session.getDefaultInstance(props,
+                     new javax.mail.Authenticator() {
+                          protected PasswordAuthentication getPasswordAuthentication() 
+                          {
+                                return new PasswordAuthentication("cannabiranha@gmail.com", "12345678Mn");
+                          }
+                     });
+         /** Ativa Debug para sessão */
+         session.setDebug(true);
+         try {
+
+               Message message = new MimeMessage(session);
+               message.setFrom(new InternetAddress("cannabiranha@gmail.com")); //Remetente
+
+               Address[] toUser = InternetAddress //Destinatário(s)
+                          .parse(address);  
+               message.setRecipients(Message.RecipientType.TO, toUser);
+               message.setSubject("Vagabundas");//Assunto
+               message.setText("HEY, PEPITAAAAA");
+               /**Método para enviar a mensagem criada*/
+               Transport.send(message);
+               System.out.println("Feito!!!");
+          } catch (MessagingException e) {
+               throw new RuntimeException(e);
+         }
+	}
       public static void main(String[] args) {
             Properties props = new Properties();
             /** Parâmetros de conexão com servidor Gmail */
